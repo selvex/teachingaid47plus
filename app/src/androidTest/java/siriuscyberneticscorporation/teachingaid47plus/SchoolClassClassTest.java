ee2f9f48a877137ad0f62c6c3a6d56352f7a6fcf
Johannes <johannes.schulte@student.tugraz.at>
2016-04-29 15:17:47 +0200

    [JS, PW] add SchoolClass class

---
+++ app/src/androidTest/java/siriuscyberneticscorporation/teachingaid47plus/SchoolClassClassTest.java
@@ -0,0 +1,63 @@
+package siriuscyberneticscorporation.teachingaid47plus;
+
+import junit.framework.TestCase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Johannes on 29.04.2016.
+ */
+public class SchoolClassClassTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+
+    }
+
+    public void tearDown() throws Exception {
+
+    }
+
+    public void testSchoolClass(){
+        SchoolClass schoolClass = new SchoolClass();
+    }
+
+    public void testSchoolClass2(){
+        Student peter = new Student("peter", "Adi", "0123456789", "asdf@jklö.com", "Graz", "Hallo");
+        Student josef = new Student("josef", "Bettina", "0123", "asdf@jklö.com", "Graz", "Hallo");
+        List<Student> students = new ArrayList<Student>();
+        students.add(peter);
+        students.add(josef);
+        SchoolClass schoolClass = new SchoolClass("4a", "Huber", students, "Note of Class");
+    }
+    public void testGetter(){
+        Student peter = new Student("peter", "Adi", "0123456789", "asdf@jklö.com", "Graz", "Hallo");
+        Student josef = new Student("josef", "Bettina", "0123", "asdf@jklö.com", "Graz", "Hallo");
+        List<Student> students = new ArrayList<Student>();
+        students.add(peter);
+        students.add(josef);
+        SchoolClass schoolClass = new SchoolClass("4a", "Huber", students, "Note of Class");
+
+        schoolClass.getName();
+        schoolClass.getClassTeacher();
+        schoolClass.getStudents();
+        schoolClass.getNote();
+
+    }
+
+    public void testSetter(){
+        SchoolClass schoolClass = new SchoolClass();
+        Student peter = new Student("peter", "Adi", "0123456789", "asdf@jklö.com", "Graz", "Hallo");
+        Student josef = new Student("josef", "Bettina", "0123", "asdf@jklö.com", "Graz", "Hallo");
+        List<Student> students = new ArrayList<Student>();
+        students.add(peter);
+        students.add(josef);
+
+        schoolClass.setName("4a");
+        schoolClass.setClassTeacher("Hoellerbauer");
+        schoolClass.setStudents(students);
+        schoolClass.setNote("New Note of Class");
+
+    }
+}
---
+++ app/src/main/java/siriuscyberneticscorporation/teachingaid47plus/SchoolClass.java
@@ -0,0 +1,57 @@
+package siriuscyberneticscorporation.teachingaid47plus;
+
+import java.util.List;
+
+/**
+ * Created by Johannes on 29.04.2016.
+ */
+public class SchoolClass {
+    private String name;
+    private String classTeacher;
+    private List<Student> students;
+    private String note;
+
+    /* Fehlt noch Fach*/
+
+    public SchoolClass(String name, String classTeacher, List<Student> students, String note) {
+        this.name = name;
+        this.classTeacher = classTeacher;
+        this.students = students;
+        this.note = note;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getClassTeacher() {
+        return classTeacher;
+    }
+
+    public void setClassTeacher(String classTeacher) {
+        this.classTeacher = classTeacher;
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public SchoolClass() {
+    }
+}
