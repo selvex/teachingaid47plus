99c35fa9beaac60b399877c93e90483865db5506
SeLveX Fu <selvex@users.noreply.github.com>
2016-05-04 14:27:13 +0200

    [KH,JS] finish databaselayout of student

--- app/src/androidTest/java/siriuscyberneticscorporation/teachingaid47plus/uitest/MainActivityTest.java
+++ app/src/androidTest/java/siriuscyberneticscorporation/teachingaid47plus/uitest/MainActivityTest.java
@@ -1,5 +1,6 @@
 package siriuscyberneticscorporation.teachingaid47plus.uitest;
 
+import android.content.Context;
 import android.test.ActivityInstrumentationTestCase2;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,6 +13,8 @@ import junit.framework.TestCase;
 import siriuscyberneticscorporation.teachingaid47plus.AddStudentActivity;
 import siriuscyberneticscorporation.teachingaid47plus.MainActivity;
 import siriuscyberneticscorporation.teachingaid47plus.R;
+import siriuscyberneticscorporation.teachingaid47plus.Student;
+import siriuscyberneticscorporation.teachingaid47plus.db_tables.DatabaseHelper;
 
 
 /**
@@ -48,5 +51,24 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2 {
         mySolo.clickOnView(getActivity().findViewById(R.id.action_add_subject));
         mySolo.goBack();
 
+    }
+
+    public void testDatabaseBasic()
+    {
+        Context context = getActivity().getBaseContext();
+        DatabaseHelper db = new DatabaseHelper(context);
+        Student peter = new Student("peter", "Adi", "0123456789", "asdf@jklö.com", "Graz", "Hallo");
+        Student michael = new Student("michael", "Adi", "0123456789", "asdf@jklö.com", "Graz", "Hallo");
+        long id_1 = db.createStudent(michael);
+        long id_2 = db.createStudent(peter);
+        System.out.println("Michaels new id: " + Long.toString(id_1));
+    }
+
+    public void testDatabaseEntries()
+    {
+        Context context = getActivity().getBaseContext();
+        DatabaseHelper db = new DatabaseHelper(context);
+        Student michael = db.getStudentById(1);
+        assertEquals("michael", michael.getName());
     }
 }
