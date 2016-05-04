c998c3edf7326faf02ce6d9e8c8f09a8d1f16d79
Bettina <bettina.hoellerbauer@student.tugraz.at>
2016-05-04 16:22:11 +0200

    [PW,BH] Add NewClassActivity-UI

--- app/src/main/java/siriuscyberneticscorporation/teachingaid47plus/NewClassActivity.java
+++ app/src/main/java/siriuscyberneticscorporation/teachingaid47plus/NewClassActivity.java
@@ -1,13 +1,31 @@
 package siriuscyberneticscorporation.teachingaid47plus;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class NewClassActivity extends AppCompatActivity {
+public class NewClassActivity extends AppCompatActivity implements View.OnClickListener{
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    private Button buttonDone;
+
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_class);
+
+        buttonDone = (Button) findViewById(R.id.done_button);
+
+        buttonDone.setOnClickListener(this);
+    }
+
+    public void onClick(View v) {
+        Button clickedButton = (Button) v;
+
+        if (clickedButton.getId() == R.id.done_button) {
+            Intent intent = new Intent(NewClassActivity.this, AddStudentsActivity.class);
+            startActivity(intent);
+        }
+
     }
 }
