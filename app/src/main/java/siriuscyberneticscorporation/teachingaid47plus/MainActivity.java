545c7fb7b4c419e517c5a0c3ce5c92033ae0f149
Bettina <bettina.hoellerbauer@student.tugraz.at>
2016-05-13 17:43:57 +0200

    [BH, KH] implementing story card 17,18

--- app/src/main/java/siriuscyberneticscorporation/teachingaid47plus/MainActivity.java
+++ app/src/main/java/siriuscyberneticscorporation/teachingaid47plus/MainActivity.java
@@ -6,16 +6,55 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Iterator;
 
-    @Override
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    private Spinner classDropdown;
+    private Spinner subjectDropdown;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ActionBar wat = getSupportActionBar();
         wat.show();
+
+        classDropdown = (Spinner) findViewById(R.id.class_spinner);
+        subjectDropdown = (Spinner) findViewById(R.id.subject_spinner);
+
+        classDropdown.setOnItemSelectedListener(this);
+        subjectDropdown.setOnItemSelectedListener(this);
+        Iterator<SchoolClass> classes = SchoolClass.findAll(SchoolClass.class);
+        Iterator<Subject> subjects = Subject.findAll(Subject.class);
+        ArrayList<String> classArray = new ArrayList<String>();
+        ArrayList<String> subjectArray = new ArrayList<String>();
+        while(classes.hasNext())
+        {
+            classArray.add(classes.next().getName());
+        }
+        while(subjects.hasNext())
+        {
+            subjectArray.add(subjects.next().getName());
+        }
+        fillClassDropdown(classArray);
+        fillSubjectsDropdown(subjectArray);
     }
+
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String selectedItem = parent.getItemAtPosition(position).toString();
+
+    }
+
+    public void onNothingSelected(AdapterView<?> arg0) {
+        //TODO Auto-generated method stub
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -41,6 +80,16 @@ public class MainActivity extends AppCompatActivity {
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    public void fillClassDropdown(ArrayList<String> classes) {
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, classes);
+        classDropdown.setAdapter(adapter1);
+    }
+
+    public void fillSubjectsDropdown(ArrayList<String> subjects) {
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, subjects);
+        subjectDropdown.setAdapter(adapter2);
     }
 }
 
