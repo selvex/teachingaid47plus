017797a6a0d443d4c74a9c8c037bdf6d4abe10cf
SeLveX <selvex@users.noreply.github.com>
2016-05-17 10:29:54 +0200

    [KH,JS] implement schooltestclass

--- app/src/main/java/siriuscyberneticscorporation/teachingaid47plus/SchoolTest.java
+++ app/src/main/java/siriuscyberneticscorporation/teachingaid47plus/SchoolTest.java
@@ -1,7 +1,72 @@
 package siriuscyberneticscorporation.teachingaid47plus;
 
+import com.orm.SugarRecord;
+import com.orm.dsl.Ignore;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * Created by SeLveX on 17.05.2016.
  */
-public class SchoolTest {
+public class SchoolTest extends SugarRecord {
+    private Date testDate;
+    private Student student;
+    private int rating;
+    private Subject subject;
+    @Ignore
+    private SimpleDateFormat dateFormat;
+
+
+    public SchoolTest() {
+    }
+
+    public SchoolTest(Date testDate, Student student, Subject subject) {
+        this.testDate = testDate;
+        this.student = student;
+        this.subject = subject;
+        this.dateFormat = new SimpleDateFormat("dd.MM.yyyy");
+    }
+
+    public String getTestDate() {
+        return dateFormat.format(testDate);
+    }
+
+    public void setTestDate(String testDate) throws ParseException {
+        try {
+            this.testDate = dateFormat.parse(testDate);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            this.testDate = dateFormat.parse("1.1.1111");
+        }
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public Subject getSubject() {
+        return subject;
+    }
+
+    public void setSubject(Subject subject) {
+        this.subject = subject;
+    }
+
+    public SimpleDateFormat getDateFormat() {
+        return dateFormat;
+    }
 }
