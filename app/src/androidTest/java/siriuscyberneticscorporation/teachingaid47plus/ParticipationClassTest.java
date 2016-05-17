ff6727d8cd0c3d1d437b6f37c503ce2a2a4f8375
SeLveX <selvex@users.noreply.github.com>
2016-05-17 10:32:12 +0200

    [KH,JS] create participation class test

---
+++ app/src/androidTest/java/siriuscyberneticscorporation/teachingaid47plus/ParticipationClassTest.java
@@ -0,0 +1,22 @@
+package siriuscyberneticscorporation.teachingaid47plus;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by Kevin on 29.04.2016.
+ */
+public class ParticipationClassTest extends TestCase {
+
+    public void testClass2()
+    {
+
+    }
+
+    public void testGetter() {
+
+    }
+
+    public void testSetter() {
+
+    }
+}
