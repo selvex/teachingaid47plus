37adf44820b62fa5c2a05ea73f8ce5bb735d544c
Johannes <johannes.schulte@student.tugraz.at>
2016-05-17 12:08:19 +0200

    [JS,KH]add HomeworkClass testcases

--- app/src/androidTest/java/siriuscyberneticscorporation/teachingaid47plus/HomeworkClassTest.java
+++ app/src/androidTest/java/siriuscyberneticscorporation/teachingaid47plus/HomeworkClassTest.java
@@ -13,20 +13,36 @@ public class HomeworkClassTest extends TestCase {
     }
 
     public void testHomeworkClass2(){
-    
-        SchoolClass to_enter = new SchoolClass("1Stern", "Peter Lustig", "der wird niemals alt");
-        Subject mathematic = new Subject("Mathematic", to_enter);
+
+        SchoolClass schoolClass = new SchoolClass("2Stern", "Peter Lustig", "der wird niemals alt");
+        Student gustav = new Student("Gustav", "Adi", "0123456789", "asdf@jklö.com", "Graz", "Hallo", schoolClass);
+        Subject mathematic = new Subject("Chinesisch", schoolClass);
+        String date = "22.22.2022";
+
+        Homework homework = new Homework(date, gustav, mathematic, 3, "Hallo");
     }
 
     public void testGetter(){
-        SchoolClass to_enter = new SchoolClass("1Stern", "Peter Lustig", "der wird niemals alt");
-        Subject mathematic = new Subject("Mathematic", to_enter);
-        assertEquals(mathematic.getName(),"Mathematic");
+        SchoolClass schoolClass = new SchoolClass("2Stern", "Peter Lustig", "der wird niemals alt");
+        Student gustav = new Student("Gustav", "Adi", "0123456789", "asdf@jklö.com", "Graz", "Hallo", schoolClass);
+        Subject chinesisch = new Subject("Chinesisch", schoolClass);
+        String date = "22.22.2022";
+        Homework homework = new Homework(date, gustav, chinesisch, EXCELLENT, "Hallo");
+
+        assertEquals(homework.getDate(),"22.22.2022" );
+        assertEquals(homework.getStudent().getName(), "Gustav");
+        assertEquals(homework.getSubject().getName(), "Chinesisch");
+        assertEquals(homework.getTag(), EXCELENT);
     }
 
     public void testSetter(){
-        Subject mathematic = new Subject();
-        mathematic.setName("Math");
-        assertEquals(mathematic.getName(),"Math");
+        Homework homework = new Homework();
+        SchoolClass schoolClass = new SchoolClass("2Stern", "Peter Lustig", "der wird niemals alt");
+        Student michael = new Student("michael", "Adi", "0123456789", "asdf@jklö.com", "Graz", "Hallo", schoolClass);
+        Subject chinesisch = new Subject("Chinesisch", schoolClass);
+        homework.setDate("11.11.1111");
+        homework.setStudent(michael);
+        homework.setSubject(chinesisch);
+        homework.setTag(Tags.EXCELLENT);
     }
 }
