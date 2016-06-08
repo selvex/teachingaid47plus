81ba76fa865ff657dd255c8ec7f494e075509d2a
Bettina <bettina.hoellerbauer@student.tugraz.at>
2016-06-08 12:26:53 +0200

    [BH, JS] Change Settings Activity and create ListSchoolClassesActivity and EditSchoolClassActivity

---
+++ app/src/androidTest/java/siriuscyberneticscorporation/teachingaid47plus/uitest/SettingsActivityTest.java
@@ -0,0 +1,40 @@
+package siriuscyberneticscorporation.teachingaid47plus.uitest;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+import com.robotium.solo.Solo;
+
+import junit.framework.TestCase;
+
+import siriuscyberneticscorporation.teachingaid47plus.SettingsActivity;
+
+
+/**
+ * Created by Bettina on 08.06.2016.
+ */
+public class SettingsActivityTest extends ActivityInstrumentationTestCase2 {
+
+    private Solo mySolo;
+
+    public SettingsActivityTest()
+    {
+        super(SettingsActivity.class);
+    }
+
+    public void setUp() throws Exception {
+        super.setUp();
+        mySolo = new Solo(getInstrumentation(), getActivity());
+    }
+
+    public void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    public void testClickButtons() {
+        mySolo.clickOnButton("Edit class");
+        mySolo.goBack();
+        mySolo.assertCurrentActivity("wrong activity", SettingsActivity.class);
+        mySolo.clickOnButton("Change Password");
+
+    }
+}
--- app/src/main/java/siriuscyberneticscorporation/teachingaid47plus/SettingsActivity.java
+++ app/src/main/java/siriuscyberneticscorporation/teachingaid47plus/SettingsActivity.java
@@ -1,13 +1,40 @@
 package siriuscyberneticscorporation.teachingaid47plus;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button buttonEditClass;
+    private Button buttonChangePwd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        buttonChangePwd = (Button) findViewById(R.id.change_pwd_button);
+        buttonEditClass = (Button) findViewById(R.id.edit_class_button);
+        buttonChangePwd.setBackgroundResource(R.drawable.mybutton);
+        buttonEditClass.setBackgroundResource(R.drawable.mybutton);
+
+        buttonEditClass.setOnClickListener(this);
+        buttonChangePwd.setOnClickListener(this);
+    }
+
+    public void onClick(View v){
+        Button clickedButton = (Button) v;
+
+        if(clickedButton.getId() == R.id.change_pwd_button){
+            //input dialog
+        }
+        else if(clickedButton.getId() == R.id.edit_class_button){
+            Intent intent = new Intent(SettingsActivity.this, ListSchoolClassesActivity.class);
+            startActivity(intent);
+        }
+
     }
 }
