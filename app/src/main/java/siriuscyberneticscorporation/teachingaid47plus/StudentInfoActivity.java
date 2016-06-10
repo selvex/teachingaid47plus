d3e9c2085a5d7beb34f85b2a0a46cb797d03fc21
Johannes <johannes.schulte@student.tugraz.at>
2016-06-10 12:31:13 +0200

    [JS,PW] redesign student info activity

--- app/src/main/java/siriuscyberneticscorporation/teachingaid47plus/StudentInfoActivity.java
+++ app/src/main/java/siriuscyberneticscorporation/teachingaid47plus/StudentInfoActivity.java
@@ -3,16 +3,12 @@ package siriuscyberneticscorporation.teachingaid47plus;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class StudentInfoActivity extends AppCompatActivity {
 
-    private TextView StudentNameTextView;
-    private TextView ContactPersonNameTextView;
-    private TextView ContactPersonPhoneTextView;
-    private TextView ContactPersonEmailTextView;
-    private TextView AddressTextView;
-    private TextView NoteTextView;
+
 
     private TextView StudentNameLabel;
     private TextView ContactPersonNameLabel;
@@ -21,17 +17,26 @@ public class StudentInfoActivity extends AppCompatActivity {
     private TextView AddressLabel;
     private TextView NoteLabel;
 
+    private EditText StudentNameEditText;
+    private EditText ContactPersonNameEditText;
+    private EditText ContactPersonPhoneEditText;
+    private EditText ContactPersonEmailEditText;
+    private EditText StudentAddressEditText;
+    private EditText StudentNoteEditText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_info);
 
-        StudentNameTextView = (TextView) findViewById(R.id.student_name_textview);
-        ContactPersonNameTextView = (TextView) findViewById(R.id.contact_person_name_textview);
-        ContactPersonPhoneTextView = (TextView) findViewById(R.id.contact_person_phone_textview);
-        ContactPersonEmailTextView = (TextView) findViewById(R.id.contact_person_email_textview);
-        AddressTextView = (TextView) findViewById(R.id.address_textview);
-        NoteTextView = (TextView) findViewById(R.id.note_textview);
+        System.out.println("OnCreate");
+
+        StudentNameEditText = (EditText) findViewById(R.id.name_edittext);
+        ContactPersonNameEditText = (EditText) findViewById(R.id.cpname_edittext);
+        ContactPersonPhoneEditText = (EditText) findViewById(R.id.cpphone_edittext);
+        ContactPersonEmailEditText = (EditText) findViewById(R.id.cpemail_edittext);
+        StudentAddressEditText = (EditText) findViewById(R.id.adress_edittext);
+        StudentNoteEditText = (EditText) findViewById(R.id.note_edittext);
 
         StudentNameLabel = (TextView) findViewById(R.id.student_name_label);
         ContactPersonNameLabel = (TextView) findViewById(R.id.contact_person_name_label);
@@ -39,42 +44,27 @@ public class StudentInfoActivity extends AppCompatActivity {
         ContactPersonEmailLabel = (TextView) findViewById(R.id.contact_person_email_label);
         AddressLabel = (TextView) findViewById(R.id.address_lable);
         NoteLabel = (TextView) findViewById(R.id.note_label);
+        System.out.println("sadfasdfasdfasdf");
+
 
         long StudentID = getIntent().getLongExtra("default", 0);
 
         if(StudentID == 0) {
             return;
         }
-
+        System.out.println("Intentintent");
         Student studentToShow = Student.findById(Student.class, StudentID);
 
-        if(studentToShow.getContactPersonName() == null || studentToShow.getContactPersonName().isEmpty()) {
-            ContactPersonNameTextView.setVisibility(View.GONE);
-            ContactPersonNameLabel.setVisibility(View.GONE);
-        }
-        if(studentToShow.getContactPersonTelNumber() == null || studentToShow.getContactPersonTelNumber().isEmpty()) {
-            ContactPersonPhoneTextView.setVisibility(View.GONE);
-            ContactPersonPhoneLabel.setVisibility(View.GONE);
-        }
-        if(studentToShow.getContactPersonEMail() == null || studentToShow.getContactPersonEMail().isEmpty()) {
-            ContactPersonEmailTextView.setVisibility(View.GONE);
-            ContactPersonEmailLabel.setVisibility(View.GONE);
-        }
-        if(studentToShow.getAddress() == null || studentToShow.getAddress().isEmpty()) {
-            AddressTextView.setVisibility(View.GONE);
-            AddressLabel.setVisibility(View.GONE);
-        }
-        if (studentToShow.getNote() == null || studentToShow.getNote().isEmpty()) {
-            NoteTextView.setVisibility(View.GONE);
-            NoteLabel.setVisibility(View.GONE);
-        }
 
-        StudentNameTextView.setText(studentToShow.getName());
-        ContactPersonNameTextView.setText(studentToShow.getContactPersonName());
-        ContactPersonPhoneTextView.setText(studentToShow.getContactPersonTelNumber());
-        ContactPersonEmailTextView.setText(studentToShow.getContactPersonEMail());
-        AddressTextView.setText(studentToShow.getAddress());
-        NoteTextView.setText(studentToShow.getNote());
+
+        StudentNameEditText.setText(studentToShow.getName());
+        ContactPersonNameEditText.setText(studentToShow.getContactPersonName());
+        ContactPersonPhoneEditText.setText(studentToShow.getContactPersonTelNumber());
+        ContactPersonEmailEditText.setText(studentToShow.getContactPersonEMail());
+        StudentAddressEditText.setText(studentToShow.getAddress());
+        StudentNoteEditText.setText(studentToShow.getNote());
+        System.out.println("ganz unten noch");
+
 
     }
 }
