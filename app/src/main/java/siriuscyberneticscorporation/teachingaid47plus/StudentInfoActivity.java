7f208d4e1ec0179b9b2e55141764ec06e1d1d907
Johannes <johannes.schulte@student.tugraz.at>
2016-06-10 15:24:23 +0200

    [JS,PW] Student-Info-Activity

--- app/src/main/java/siriuscyberneticscorporation/teachingaid47plus/StudentInfoActivity.java
+++ app/src/main/java/siriuscyberneticscorporation/teachingaid47plus/StudentInfoActivity.java
@@ -1,21 +1,27 @@
 package siriuscyberneticscorporation.teachingaid47plus;
 
+import android.content.Intent;
+import android.provider.Telephony;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class StudentInfoActivity extends AppCompatActivity {
+import org.w3c.dom.Text;
+
+import java.util.List;
+
+public class StudentInfoActivity extends AppCompatActivity implements View.OnClickListener{
 
 
+    private Button buttonSave;
+    private Button buttonDelete;
 
-    private TextView StudentNameLabel;
-    private TextView ContactPersonNameLabel;
-    private TextView ContactPersonPhoneLabel;
-    private TextView ContactPersonEmailLabel;
-    private TextView AddressLabel;
-    private TextView NoteLabel;
+    private TextView plusTextView;
+    private TextView minusTextView;
+    private TextView testAverageTextView;
 
     private EditText StudentNameEditText;
     private EditText ContactPersonNameEditText;
@@ -24,12 +30,15 @@ public class StudentInfoActivity extends AppCompatActivity {
     private EditText StudentAddressEditText;
     private EditText StudentNoteEditText;
 
+    Student studentToShow = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_info);
 
-        System.out.println("OnCreate");
+        buttonSave = (Button) findViewById(R.id.save_button);
+        buttonDelete = (Button) findViewById(R.id.delete_button);
 
         StudentNameEditText = (EditText) findViewById(R.id.name_edittext);
         ContactPersonNameEditText = (EditText) findViewById(R.id.cpname_edittext);
@@ -38,22 +47,20 @@ public class StudentInfoActivity extends AppCompatActivity {
         StudentAddressEditText = (EditText) findViewById(R.id.adress_edittext);
         StudentNoteEditText = (EditText) findViewById(R.id.note_edittext);
 
-        StudentNameLabel = (TextView) findViewById(R.id.student_name_label);
-        ContactPersonNameLabel = (TextView) findViewById(R.id.contact_person_name_label);
-        ContactPersonPhoneLabel = (TextView) findViewById(R.id.contact_person_phone_label);
-        ContactPersonEmailLabel = (TextView) findViewById(R.id.contact_person_email_label);
-        AddressLabel = (TextView) findViewById(R.id.address_lable);
-        NoteLabel = (TextView) findViewById(R.id.note_label);
-        System.out.println("sadfasdfasdfasdf");
+        plusTextView = (TextView) findViewById(R.id.plus_textview);
+        minusTextView = (TextView) findViewById(R.id.minus_textview);
+        testAverageTextView = (TextView) findViewById(R.id.test_average_textview);
 
+
+        buttonSave.setOnClickListener(this);
+        buttonDelete.setOnClickListener(this);
 
         long StudentID = getIntent().getLongExtra("default", 0);
 
         if(StudentID == 0) {
             return;
         }
-        System.out.println("Intentintent");
-        Student studentToShow = Student.findById(Student.class, StudentID);
+        studentToShow = Student.findById(Student.class, StudentID);
 
 
 
@@ -63,8 +70,72 @@ public class StudentInfoActivity extends AppCompatActivity {
         ContactPersonEmailEditText.setText(studentToShow.getContactPersonEMail());
         StudentAddressEditText.setText(studentToShow.getAddress());
         StudentNoteEditText.setText(studentToShow.getNote());
-        System.out.println("ganz unten noch");
 
+        int sum_plus = 0;
+        int sum_minus = 0;
+        int average_points = 0;
+        List<Participation> participations = Participation.find(Participation.class, "student = ?", String.valueOf(studentToShow.getId()));
+        for (Participation p: participations) {
+            if(p.getRating() > 0) {
+                sum_plus = sum_plus + p.getRating();
+            }
+            else if(p.getRating() < 0){
+                sum_minus = sum_minus - p.getRating();
+            }
+        }
 
+        List<SchoolTest> tests = SchoolTest.find(SchoolTest.class, "student = ?", String.valueOf(studentToShow.getId()));
+        for (SchoolTest s: tests) {
+            average_points = average_points + s.getRating();
+        }
+        if(tests.size()>0) {
+            average_points = (average_points / tests.size());
+        }
+        plusTextView.setText("Plus: " + sum_plus);
+        minusTextView.setText("Minus: " + sum_minus);
+        testAverageTextView.setText("Testpoints average: " + average_points);
+    }
+
+    public void onClick(View v) {
+
+        Button clickedButton = (Button) v;
+
+        if (clickedButton.getId() == R.id.save_button) {
+
+            Intent intent = new Intent(StudentInfoActivity.this, MainActivity.class);
+
+            Student newStudent = Student.find(Student.class, "name = ?", studentToShow.getName()).get(0);
+            newStudent.setName(StudentNameEditText.getText().toString());
+            newStudent.setContactPersonName(ContactPersonNameEditText.getText().toString());
+            newStudent.setContactPersonTelNumber(ContactPersonPhoneEditText.getText().toString());
+            newStudent.setContactPersonEMail(ContactPersonEmailEditText.getText().toString());
+            newStudent.setAddress(StudentAddressEditText.getText().toString());
+            newStudent.setNote(StudentNoteEditText.getText().toString());
+
+            newStudent.save();
+            startActivity(intent);
+
+        }
+
+        if (clickedButton.getId() == R.id.delete_button) {
+
+            Intent intent = new Intent(StudentInfoActivity.this, MainActivity.class);
+
+            List<Participation> participations = Participation.find(Participation.class, "student = ?", String.valueOf(studentToShow.getId()));
+            for (Participation p: participations) {
+                p.delete();
+            }
+            List<SchoolTest> tests = SchoolTest.find(SchoolTest.class, "student = ?", String.valueOf(studentToShow.getId()));
+            for (SchoolTest s: tests) {
+                s.delete();
+            }
+            List<Homework> homeworks = Homework.find(Homework.class, "student = ?", String.valueOf(studentToShow.getId()));
+            for (Homework h: homeworks) {
+                h.delete();
+            }
+            Student newStudent = Student.find(Student.class, "name = ?", studentToShow.getName()).get(0);
+            newStudent.delete();
+            startActivity(intent);
+        }
     }
 }
